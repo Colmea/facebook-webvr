cbe72a06f5e7d4abb5d6e47fea7975c6f840500f
Arthur Schwaiger <arthur.schwaiger@gmail.com>
2016-05-07 15:38:31 +0200

    Add npm dependencies + create default config for webpack

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    debug: false,
+    devtool: 'source-map',
+    entry: [
+        'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+        'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+        './src/main.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        alias:{
+            app: path.resolve( __dirname, './src/app'),
+            components: path.resolve( __dirname, './src/app/components'),
+        },
+        extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx']
+    },
+    eslint: {
+        configFile: './.eslintrc'
+    },
+    module: {
+        preLoaders: [
+            {
+                test: /\.jsx?$/,
+                loader: "eslint-loader",
+                exclude: /node_modules/,
+            },
+        ],
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'react-hot',
+                exclude: /node_modules/
+            },
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    plugins: ["transform-class-properties"],
+                    presets: ['es2015', 'react', 'react-hmre']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
