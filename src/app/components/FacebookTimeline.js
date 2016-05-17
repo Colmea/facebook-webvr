f2a67725815d42e417483b6b917a172f8dccbf28
Arthur Schwaiger <arthur.schwaiger@gmail.com>
2016-05-17 23:49:37 +0200

    FacebookTimeline: use new Post component

--- src/app/components/FacebookTimeline.js
+++ src/app/components/FacebookTimeline.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import THREE from 'three';
 import Html3D from './utils/Html3D';
-
+// Material UI
 import List from 'material-ui/lib/lists/list';
 import ListItem from 'material-ui/lib/lists/list-item';
 import Avatar from 'material-ui/lib/avatar';
@@ -12,53 +12,18 @@ import RaisedButton from 'material-ui/lib/raised-button';
 import FontIcon from 'material-ui/lib/font-icon';
 import AccountFace from 'material-ui/lib/svg-icons/action/account-circle';
 
+import Post from './facebook/Post';
 
 export default class FacebookTimeline extends React.Component
 {
-    onClick = () => {
-        alert('on click');
-    }
-
     render() {
 
-        let rotationY = (this.props.frameNumber % 720)/2;
+        let rotationY = (this.props.frameNumber % (360 * 10))/10;
         let rotation = new THREE.Euler(0, rotationY*Math.PI/180, 0);
 
         return (
             <group ref="facebook-timeline">
-                <Html3D ref="html3D" rotation={rotation}>
-                    <div>
-                        <List ref="button" subheader="Timeline">
-                            <ListItem
-                                ref="listItem"
-                                key={0}
-                                primaryText="Heey that's working, huh ?"
-                                secondaryText="from Colmea"
-                                initiallyOpen={true}
-                                primaryTogglesNestedList={true}
-                                leftAvatar={<Avatar
-                                icon={<AccountFace />}
-                                color={Colors.blue200}
-                                backgroundColor={Colors.grey200}
-                                onClick={this.onClick}
-                            />}
-                            />
-                            <ListItem
-                                ref="listItem"
-                                key={1}
-                                primaryText="Hello World !"
-                                secondaryText="from Colmea"
-                                initiallyOpen={true}
-                                primaryTogglesNestedList={true}
-                                leftAvatar={<Avatar
-                              icon={<AccountFace />}
-                              color={Colors.blue200}
-                              backgroundColor={Colors.grey200}
-                            />}
-                            />
-                        </List>
-                    </div>
-                </Html3D>
+                <Post position={new THREE.Vector3(0, 100, -100)} rotation={rotation} />
             </group>
         );
     }
