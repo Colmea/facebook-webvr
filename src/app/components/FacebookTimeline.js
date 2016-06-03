2d5c1180939b027d2348385a3f7453780e509d02
Arthur Schwaiger <arthur.schwaiger@gmail.com>
2016-06-03 09:29:18 +0200

    FacebookTimeline: add custom content (video, iframe, html5 game) + temporary remove rotation

--- src/app/components/FacebookTimeline.js
+++ src/app/components/FacebookTimeline.js
@@ -7,13 +7,22 @@ export default class FacebookTimeline extends React.Component
     render() {
 
         let rotationY = (this.props.frameNumber % (360 * 10))/10;
-        let rotation = new THREE.Euler(0, rotationY*Math.PI/180, 0);
+        let rotation = new THREE.Euler(0, 0, 0);
 
         return (
             <group ref="facebook-timeline">
-                <Post position={new THREE.Vector3(0, 100, -100)} rotation={rotation} />
-                <Post position={new THREE.Vector3(600, 300, -1000)} rotation={rotation} />
-                <Post position={new THREE.Vector3(-800, 500, -800)} rotation={rotation} />
+                <Post position={new THREE.Vector3(0, 100, -100)} rotation={rotation}>
+                    Foo Fighters FTW <br /><br />
+                    <iframe width="465" height="315" src="http://localhost:3000" frameborder="0" allowfullscreen></iframe>
+                </Post>
+                <Post position={new THREE.Vector3(600, 300, -1000)} rotation={rotation}>
+                    <iframe width="465" height="315" src="http://gopherwoodstudios.com/sandtrap/sand-trap.htm" frameborder="0" allowfullscreen></iframe>
+                </Post>
+                <Post position={new THREE.Vector3(-800, 500, -800)} rotation={rotation}>
+                    Foo Fighter FTW<br /><br />
+                    <iframe width="465" height="315" src="https://www.youtube.com/embed/SBjQ9tuuTJQ" frameborder="0" allowfullscreen></iframe>
+
+                </Post>
             </group>
         );
     }
