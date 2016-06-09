fd990f22b9a9763c3a282cae9d1914078008f27c
Arthur Schwaiger <arthur.schwaiger@gmail.com>
2016-06-09 23:54:31 +0200

    FacebookTimeline: perform fb login on load + create FB box when connected

--- src/app/components/FacebookTimeline.js
+++ src/app/components/FacebookTimeline.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import THREE from 'three';
+
+import FacebookActions from 'actions/FacebookActions';
+
+import LoginStatus from './facebook/LoginStatus';
 import Post from './facebook/Post';
 
 export default class FacebookTimeline extends React.Component
 {
     static propTypes = {
         frameNumber: React.PropTypes.number,
-        posts: React.PropTypes.array
+        FacebookStore: React.PropTypes.object,
+        PostStore: React.PropTypes.object
     };
 
     static defaultProps = {
-        frameNumber: 0,
-        posts: []
+        frameNumber: 0
     };
+
+    componentDidMount() {
+        setTimeout(() => {
+            FacebookActions.login();
+        }, 2000);
+    }
 
     render() {
         let rotationY = (this.props.frameNumber % (360 * 10)) / 4000;
@@ -21,7 +31,7 @@ export default class FacebookTimeline extends React.Component
         let posts = [];
 
         // Create Facebook posts
-        this.props.posts.forEach((post, index) => {
+        this.props.PostStore.posts.forEach((post, index) => {
             posts.push(
                 <Post
                     key={post.id}
@@ -35,6 +45,13 @@ export default class FacebookTimeline extends React.Component
         return (
             <group ref="facebook-timeline">
                 {posts}
+
+                <LoginStatus
+                    frameNumber={this.props.frameNumber}
+                    position={new THREE.Vector3(0, 400, 0)}
+                    rotation={rotation}
+                    isConnected={this.props.FacebookStore.connected}
+                />
             </group>
         );
     }
